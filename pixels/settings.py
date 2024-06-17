7d1feb9107ccf3580cf9a38f528e8e0cf759a1a8
jovi <ejakpoviumolo@gmail.com>
2024-06-17 01:36:47 -1100

    done

--- pixels/settings.py
+++ pixels/settings.py
@@ -15,7 +15,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 SECRET_KEY = 'django-insecure-nx0javx)r^1valaxr8j-f%^33dc2*hao8whc$0hq+f%$@h7%+@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+# DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+DEBUG = False
+
 
 # Additional security settings
 if not DEBUG:
